97a7c92f711f8f0176e793b9b266cd740ebf53b6
Jack Nolddor <1280022+nolddor@users.noreply.github.com>
2022-12-10 11:23:13 +0100

    Add winston as logger

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const SteamCardExchange = require('./lib/SteamCardExchange')
 const AppDir = require('./lib/AppDir')
 const JSONStorage = require('./lib/JSONStorage')
+const Logger = require('./lib/Logger')
 
+Logger.info("Connecting to SteamCardExchange...")
 SteamCardExchange.getBadges().then(badges => {
+    let count = Object.keys(badges).length
+    Logger.info(`Found ${count} Steam apps having trading cards.`)
     JSONStorage.store(AppDir.data(), 'badges.json', badges)
 })
