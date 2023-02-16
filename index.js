818ca4098f38036a842d7cf40f44c77d0c4d06d3
Jack Nolddor <1280022+nolddor@users.noreply.github.com>
2023-02-16 01:44:05 +0100

    Code refactor

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const AppDir = require('./lib/AppDir')
 const JSONStorage = require('./lib/JSONStorage')
 const Logger = require('./lib/Logger')
 
-Logger.info('Connecting to SteamCardExchange...')
-SteamCardExchange.getBadges().then(badges => {
+async function main() {
+    Logger.info('Connecting to SteamCardExchange...')
+    const badges = await SteamCardExchange.getBadges()
     const count = Object.keys(badges).length
     Logger.info(`Found ${count} Steam apps having trading cards.`)
+
     JSONStorage.store(AppDir.data(), 'badges.json', badges, { minify: false })
     JSONStorage.store(AppDir.data(), 'badges.min.json', badges)
-})
+}
+
+main()
