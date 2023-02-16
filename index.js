c33c05a6676c9648b4e0a332bcf2cd6dca63f904
Jack Nolddor <1280022+nolddor@users.noreply.github.com>
2023-02-16 01:24:30 +0100

    Add minification flag to `JSONStorage`

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ Logger.info('Connecting to SteamCardExchange...')
 SteamCardExchange.getBadges().then(badges => {
     const count = Object.keys(badges).length
     Logger.info(`Found ${count} Steam apps having trading cards.`)
-    JSONStorage.store(AppDir.data(), 'badges.json', badges)
+    JSONStorage.store(AppDir.data(), 'badges.json', badges, { minify: false })
+    JSONStorage.store(AppDir.data(), 'badges.min.json', badges)
 })
