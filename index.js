83a477425d7b525aaa212233a83670ca6129ce7b
Jack Nolddor <1280022+nolddor@users.noreply.github.com>
2025-05-10 00:26:59 +0200

    chore: rename sce into scrapper

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const SteamCardExchange = require('./lib/SteamCardExchange')
+const Scrapper = require('./lib/Scrapper')
 const JSONStorage = require('./lib/JSONStorage')
 const Logger = require('./lib/Logger')
 const EFolders = require('./enum/EFolders')
@@ -6,11 +6,11 @@ const EFiles = require('./enum/EFiles')
 const ELocales = require('./enum/ELocales')
 
 async function main () {
-  Logger.info('Connecting to SteamCardExchange...')
-  const sce = await SteamCardExchange.getBadges()
+  Logger.info('Connecting to remote site...')
+  const latest = await Scrapper.getBadges()
   const cache = JSONStorage.readSync(EFolders.DATA, EFiles.BADGES)
 
-  const badges = { ...cache, ...sce }
+  const badges = { ...cache, ...latest }
   const badgesSlim = Object.fromEntries(
     Object.entries(badges).map(([k, v]) => [k, v.size])
   )
